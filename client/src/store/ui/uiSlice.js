6c8259c33457c43dd680eb2ad67ac6c09e226672
Dante Tárraga <dante.tarragau@gmail.com>
2024-08-26 15:14:06 -0500

    feat: Update store module

---
+++ client/src/store/ui/uiSlice.js
@@ -0,0 +1,15 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+
+}
+
+const uiSlice = createSlice({
+  name:m´,  second,
+  initialState,
+  reducers: {}
+});
+
+export const {} = uiSlice.actions
+
+export default uiSlice.reducer
