47d271c6e061b6a66f7269b688fb24d28e0fbd9c
Dante Tárraga <dante.tarragau@gmail.com>
2024-09-01 13:50:24 -0500

    chore: Update .gitignore and store.js

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -7,5 +7,8 @@ export const store = configureStore({
   reducer: {
     ui: uiSlice.reducer,
     calendar: calendarSlice.reducer
-  }
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+    serializableCheck: false
+  })
 })
