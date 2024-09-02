6d9590af64903b7b4f521bf4658a5950048ee6bd
Dante Tárraga <dante.tarragau@gmail.com>
2024-09-02 14:43:24 -0500

    feat: Add createEvent functionality to EventsController

--- server/controllers/events.js
+++ server/controllers/events.js
@@ -4,9 +4,24 @@ class EventsController {
   static async getEvents (req, res) {
     const events = await Event.find()
 
-    res.json({
+    res.status(200).json({
       ok: true,
-      message: 'getEvents'
+      message: 'Events fetched successfully',
+      events
+    })
+  }
+
+  static async createEvent (req, res) {
+    const { title, start, end, description } = req.body
+    const user = req.user._id
+
+    const event = new Event({ title, start, end, description, user })
+    await event.save()
+
+    res.status(201).json({
+      ok: true,
+      message: 'Event created successfully',
+      event
     })
   }
 }
