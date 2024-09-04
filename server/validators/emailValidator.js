82ebb3671f2f5747a873d5c1fc318cfb68bff87d
Dante Tárraga <dante.tarragau@gmail.com>
2024-09-03 22:04:02 -0500

    chore: Remove unused server files and dependencies

--- server/validators/emailValidator.js
+++
@@ -1,8 +0,0 @@
-import { check } from 'express-validator'
-
-export const emailValidator = [
-  check('email', 'Email is required')
-    .isEmail()
-    .withMessage('Invalid email')
-    .normalizeEmail()
-]
--- server/validators/passwordValidator.js
+++
@@ -1,7 +0,0 @@
-import { check } from 'express-validator'
-
-export const passwordValidator = [
-  check('password', 'Password is required')
-    .isLength({ min: 6 })
-    .withMessage('Password must be at least 6 characters')
-]
