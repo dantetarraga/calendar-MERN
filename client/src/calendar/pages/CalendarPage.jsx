ca9bb74afb9db2b5992eae2e35d5ba94dc680435
Dante Tárraga <dante.tarragau@gmail.com>
2024-09-03 20:09:52 -0500

    feat: Implement lazy loading for calendar events

--- client/src/calendar/pages/CalendarPage.jsx
+++ client/src/calendar/pages/CalendarPage.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Calendar } from 'react-big-calendar'
 
 import 'react-big-calendar/lib/css/react-big-calendar.css'
@@ -11,9 +11,13 @@ import { CalendarEvent, CalendarModal, FabAddNew, NavBar } from '../'
 import FabDeleteEvent from '../components/FabDeleteEvent'
 
 const CalendarPage = () => {
-  const { events, setActiveEvent } = useCalendarStore()
+  const { events, setActiveEvent, startLoadingEvents } = useCalendarStore()
   const { openDateModal } = useUiStore()
   const [lastView] = useState(localStorage.getItem('lastView') || 'week')
+
+  useEffect(() => {
+    startLoadingEvents()
+  }, [])
 
   // eslint-disable-next-line no-unused-vars
   const eventStyleGetter = (event, start, end, isSelected) => {
--- client/src/hook/useCalendarStore.jsx
+++ client/src/hook/useCalendarStore.jsx
@@ -1,9 +1,11 @@
 import { useDispatch, useSelector } from 'react-redux'
 
+import { calendarApi } from '../api'
 import { onAddNewEvent, onDeleteEvent, onSetActiveEvent, onUpdateEvent } from '../store'
 
 export const useCalendarStore = () => {
   const { events, selectedEvent } = useSelector((state) => state.calendar)
+  const { user } = useSelector((state) => state.auth)
   const dispatch = useDispatch()
 
   const setActiveEvent = (calendarEvent) => {
@@ -11,16 +13,25 @@ export const useCalendarStore = () => {
   }
 
   const startSavingEvent = async (calendarEvent) => {
-    console.log(calendarEvent)
     if (calendarEvent._id) {
       dispatch(onUpdateEvent({ ...calendarEvent }))
     } else {
-      dispatch(onAddNewEvent({ ...calendarEvent, _id: new Date().toISOString() }))
+      const { data } = await calendarApi.post('/events/create', calendarEvent)
+      dispatch(onAddNewEvent({ ...calendarEvent, id: data.event.id, user }))
     }
   }
 
   const startDeletingEvent = async () => {
     dispatch(onDeleteEvent())
+  }
+
+  const startLoadingEvents = async () => {
+    try {
+      const { data } = await calendarApi.get('/events')
+      console.log(data)
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   return {
@@ -30,6 +41,7 @@ export const useCalendarStore = () => {
 
     setActiveEvent,
     startSavingEvent,
-    startDeletingEvent
+    startDeletingEvent,
+    startLoadingEvents
   }
 }
