9d2df222ab237fc4bcb4629fb6540192882c634c
Dante Tárraga <dante.tarragau@gmail.com>
2024-09-03 22:38:23 -0500

    chore: Update VITE_API_URL in .env.template and calendarApi.js

--- src/api/calendarApi.js
+++ src/api/calendarApi.js
@@ -4,6 +4,8 @@ import { getEnvVariables } from '../helpers'
 
 const { VITE_API_URL } = getEnvVariables()
 
+console.log(VITE_API_URL)
+
 const calendarApi = axios.create({
   baseURL: VITE_API_URL,
   headers: {
